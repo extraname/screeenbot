37708d3eae2ce01d1f200307a516183d297226e3
extraname <zorraider@gmail.com>
2020-06-23 14:13:01 +0300

    Refactor

--- main.py
+++ main.py
@@ -10,35 +10,46 @@ bot = telebot.TeleBot(config.token)
 
 
 def do_screenshot(username, date, url):
+    """ Script block """
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
     driver.get(url)
-    for m in get_monitors():
-        driver.set_window_size(m.width, m.height)
+    for size in get_monitors():
+        driver.set_window_size(size.width, size.height)
         el = driver.find_element_by_tag_name('body')
         el.screenshot(filename=os.path.join('media', f'{username}.{date}.png'))
         driver.quit()
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Hello, I can give you screenshot, just give me link')
 
 
+# @bot.message_handler(content_types=['text'])
+# def send_text(message):
+#     """ Main answer block"""
+#     if message.text.startswith('http://') or message.text.startswith('https://'):
+#         if validators.url(message.text):
+#             do_screenshot(message.from_user.first_name, message.date, message.text)
+#             with open(f'media/{message.from_user.first_name}.{message.date}.png', 'rb') as photo:
+#                 bot.send_photo(message.chat.id, photo=photo)
+#         else:
+#             bot.send_message(message.chat.id, 'Url is not valid')
+#     else:
+#         bot.send_message(message.chat.id, 'Please, input correct URL')
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    print(message)
-    print(message.from_user.first_name)
+    """ Main answer block"""
 
-    if message.text.startswith('http') or message.text.startswith('www'):
-        if validators.url(message.text):
-            do_screenshot(message.from_user.first_name, message.date, message.text)
-            with open(f'media/{message.from_user.first_name}.{message.date}.png', 'rb') as photo:
-                bot.send_photo(message.chat.id, photo=photo)
-        else:
-            bot.send_message(message.chat.id, 'Url is not valid')
+    if validators.url(message.text):
+        do_screenshot(message.from_user.first_name, message.date, message.text)
+        with open(f'media/{message.from_user.first_name}.{message.date}.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo=photo)
     else:
-        bot.send_message(message.chat.id, 'Please, input correct URL')
+        bot.send_message(message.chat.id, "Please input correct URL. Thank's you")
 
 
 if __name__ == '__main__':
